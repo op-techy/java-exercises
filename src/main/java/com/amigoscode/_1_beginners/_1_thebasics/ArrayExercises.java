89f115323aaa640cc802d4fa01cfde5e09376610
op-techy <opeoluwa.babalola@stu.cu.edu.ng>
2026-08-02 21:47:52 +0100

    ArrayExercises done

--- src/main/java/com/amigoscode/_1_beginners/_1_thebasics/ArrayExercises.java
+++ src/main/java/com/amigoscode/_1_beginners/_1_thebasics/ArrayExercises.java
@@ -1,5 +1,7 @@
 package com.amigoscode._1_beginners._1_thebasics;
 
+import java.util.Arrays;
+
 /**
  * Exercise: Arrays
  *
@@ -12,31 +14,54 @@ public class ArrayExercises {
 
         // TODO: 1 - Declare and initialize an int array called 'numbers' with 5 values
         // Hint: int[] numbers = {value1, value2, value3, value4, value5};
+        int[] numbers = {1,2,3,4,5};
 
 
         // TODO: 2 - Access and print the first and last elements of the array
         // Hint: Arrays are zero-indexed. First element is numbers[0].
         //       Last element is numbers[numbers.length - 1].
+        System.out.println(numbers[0]);
+        System.out.println(numbers[4]);
+        System.out.println();
 
 
         // TODO: 3 - Change the value of the third element (index 2) to 99 and print it
         // Hint: numbers[2] = 99;
+        numbers[2] = 99;
+        //  System.out.println(Arrays.toString(numbers));
+        System.out.println(numbers[2]);
+        System.out.println();
 
 
         // TODO: 4 - Get the length of the array and print it
         // Hint: Use the .length property (not a method, no parentheses)
+        System.out.println(numbers.length);
+        System.out.println();
 
 
         // TODO: 5 - Loop through the array using a standard for loop and print each element
         // Hint: for (int i = 0; i < numbers.length; i++) { ... }
+        for (int i = 0; i < numbers.length; i++){
+            System.out.println(numbers[i]);
+        }
+        System.out.println();
 
 
         // TODO: 6 - Loop through the array using an enhanced for loop (for-each) and print each element
         // Hint: for (int num : numbers) { ... }
+        for (int num : numbers){
+            System.out.println(num);
+        }
+        System.out.println();
 
 
         // TODO: 7 - Find the sum of all elements in the array and print it
         // Hint: Declare a sum variable, loop through the array, and add each element to sum
+        int sum = 0;
+        for (int num : numbers){
+            sum += num;
+        }
+        System.out.println(sum);
 
     }
 }
