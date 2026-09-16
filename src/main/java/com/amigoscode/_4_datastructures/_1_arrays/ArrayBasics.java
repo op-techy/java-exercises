ffe1cfeadc72c308f8e6e1f70e37e2b523131814
op-techy <opeoluwa.babalola@stu.cu.edu.ng>
2026-09-16 17:15:59 +0100

    Java Data Structures: Arrays

--- src/main/java/com/amigoscode/_4_datastructures/_1_arrays/ArrayBasics.java
+++ src/main/java/com/amigoscode/_4_datastructures/_1_arrays/ArrayBasics.java
@@ -10,30 +10,44 @@ public class ArrayBasics {
     public static void main(String[] args) {
 
         // TODO: 1 - Create an int array of size 5
+        int[] numbers = new int[5];
 
 
         // TODO: 2 - Fill the array with values 10, 20, 30, 40, 50 using a for loop
         //           (hint: use i * 10 + 10 or similar pattern)
-
+        for (int i = 0; i <= 4 ; i++) {
+            numbers[i] = i * 10 + 10;
+        }
 
         // TODO: 3 - Print all elements of the array using Arrays.toString()
-
+        System.out.println(Arrays.toString(numbers));
 
         // TODO: 4 - Find the maximum value in the array
         //           Iterate through the array and track the largest value found
-
+        int max = 0;
+        for (int a : numbers){
+            if( a > max){
+                max = a;
+            }
+        }
+        System.out.println("max: " + max);
 
         // TODO: 5 - Find the minimum value in the array
         //           Iterate through the array and track the smallest value found
-
+        int min = numbers[0];
+        for (int num : numbers){
+            if (min > num) min = num;
+        }
+        System.out.println("min: " + min);
 
         // TODO: 6 - Sort the array using Arrays.sort()
         //           Then print the sorted array
-
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
 
         // TODO: 7 - Use Arrays.binarySearch() to find the index of value 30
         //           Note: the array must be sorted before using binarySearch
         //           Print the index where the value was found
-
+        System.out.println(Arrays.binarySearch(numbers, 30));
     }
 }
--- src/main/java/com/amigoscode/_4_datastructures/_1_arrays/TwoDArrays.java
+++ src/main/java/com/amigoscode/_4_datastructures/_1_arrays/TwoDArrays.java
@@ -8,6 +8,7 @@ public class TwoDArrays {
     public static void main(String[] args) {
 
         // TODO: 1 - Create a 3x3 int 2D array called 'matrix'
+        int[][] matrix = new int[3][3];
 
 
         // TODO: 2 - Fill the matrix with multiplication table values
@@ -16,23 +17,57 @@ public class TwoDArrays {
         //           1 2 3
         //           2 4 6
         //           3 6 9
+        for (int i = 0; i <= 2; i++) {
+
+            for (int j = 0; j <= 2 ; j++) {
+                matrix[i][j] = (i + 1) * (j + 1);
+            }
+
+        }
 
 
         // TODO: 3 - Print the matrix in row/column format
         //           Each row on a new line, values separated by spaces or tabs
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("\t " + matrix[i][j]);
+            }
+            System.out.println();
+        }
 
 
         // TODO: 4 - Find and print the sum of all elements in the matrix
         //           Use nested loops to accumulate the total
+        int sum = 0;
+        for (int[] row : matrix){
+            for (int i : row){
+                sum += i;
+            }
+        }
+        System.out.println("The sum of all numbers in the matrix is: "  + sum);
 
 
         // TODO: 5 - Find and print the sum of row index 1 (the second row: 2, 4, 6)
         //           Iterate through all columns of that specific row
-
+        int indexSum = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if( j != 1 ) continue;
+                indexSum += matrix[i][j];
+            }
+        }
+        System.out.printf("The sum of column %s is: %d\n", 1, indexSum);
 
         // TODO: 6 - Find and print the sum of the diagonal elements (top-left to bottom-right)
         //           Diagonal elements are where row index == column index
         //           Expected: matrix[0][0] + matrix[1][1] + matrix[2][2] = 1 + 4 + 9 = 14
-
+        int diagSum = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if ( i != j ) continue;
+                diagSum += matrix[i][j];
+            }
+        }
+        System.out.printf("The sum of the diagonal elements is %d \n", diagSum);
     }
 }
