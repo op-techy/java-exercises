fec2bbd6793e3d8a7a5b210480bbb1d7813bbbbf
op-techy <opeoluwa.babalola@stu.cu.edu.ng>
2026-09-16 19:13:45 +0100

    Java Data Structures: ArrayLists

--- src/main/java/com/amigoscode/_4_datastructures/_2_arraylist/ArrayListOperations.java
+++ src/main/java/com/amigoscode/_4_datastructures/_2_arraylist/ArrayListOperations.java
@@ -13,30 +13,42 @@ public class ArrayListOperations {
 
         // TODO: 1 - Create an ArrayList of Integers called 'numbers'
         //           Add these 10 numbers: 45, 12, 78, 34, 56, 23, 89, 67, 11, 90
+        List<Integer> numbers = new ArrayList<>();
+        Collections.addAll(numbers, 45, 12, 78, 34, 56, 23, 89, 67, 11, 90 );
 
 
         // TODO: 2 - Sort the list in ascending order using Collections.sort()
         //           Print the sorted list
+        Collections.sort(numbers);
+        System.out.println(numbers);
 
 
         // TODO: 3 - Reverse the list using Collections.reverse()
         //           Print the reversed list
+        Collections.reverse(numbers);
+        System.out.println(numbers);
 
 
         // TODO: 4 - Find and print the maximum value using Collections.max()
+        int max = Collections.max(numbers);
+        System.out.println(max);
 
 
         // TODO: 5 - Create a subList containing the first 5 elements (indices 0 to 4)
         //           Print the subList
         //           Note: subList returns a view, not a copy
+        System.out.println(numbers.subList(0,4));
 
 
         // TODO: 6 - Convert the ArrayList to an Integer array using toArray()
         //           Print the array length to verify
+        Object[] array = numbers.toArray();
+        System.out.println(array.length);
 
 
         // TODO: 7 - Clear the list using clear() and verify it's empty
         //           Print the size and the result of isEmpty()
-
+        numbers.clear();
+        System.out.printf("The size of the array is %d and the result of isEmpty is %s", numbers.size(), numbers.isEmpty());
     }
 }
