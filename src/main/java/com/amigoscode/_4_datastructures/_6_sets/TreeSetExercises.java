04dceac6096340bb55cb8ce1e229fd0e5deb8286
op-techy <opeoluwa.babalola@stu.cu.edu.ng>
2026-09-21 18:57:17 +0100

    Java Data Structures: Sets

--- src/main/java/com/amigoscode/_4_datastructures/_6_sets/TreeSetExercises.java
+++ src/main/java/com/amigoscode/_4_datastructures/_6_sets/TreeSetExercises.java
@@ -10,26 +10,43 @@ public class TreeSetExercises {
     public static void main(String[] args) {
 
         // TODO: 1 - Create a TreeSet of Integers called 'numbers'
-
+        TreeSet<Integer> numbers = new TreeSet<>();
 
         // TODO: 2 - Add these elements: 50, 20, 40, 10, 30, 60, 15, 45
         //           Print the set and observe that elements are automatically sorted
+        numbers.add(50);
+        numbers.add(20);
+        numbers.add(40);
+        numbers.add(10);
+        numbers.add(30);
+        numbers.add(60);
+        numbers.add(15);
+        numbers.add(45);
+        System.out.println(numbers);
 
 
         // TODO: 3 - Get and print the first (lowest) element using first()
         //           Get and print the last (highest) element using last()
+        System.out.println(numbers.getFirst());
+        System.out.println(numbers.getLast());
 
 
         // TODO: 4 - Get a subset of elements from 20 (inclusive) to 45 (exclusive) using subSet()
         //           Print the subset
+        System.out.println(numbers.subSet(20,true,45,false));
 
 
         // TODO: 5 - Get and print the headSet (elements less than 30)
         //           Get and print the tailSet (elements greater than or equal to 30)
+        System.out.println(numbers.headSet(30,false));
+        System.out.println(numbers.tailSet(30,true));
 
 
         // TODO: 6 - Iterate over the TreeSet using a for-each loop
         //           Print each element and observe the natural ascending order
+        for (int num : numbers){
+            System.out.println(num);
+        }
 
     }
 }
